b2d96bb478c116d11f5adad20b1bef2e53be1499
LiamYoun <95008167+liam4806@users.noreply.github.com>
2023-04-30 14:42:51 +0900

    Update script.js
    Add comments for description.

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -1,4 +1,5 @@
 'use strict';
+/*Declaring selections*/
 const score1= document.querySelector("#score--0");
 const score2= document.querySelector("#score--1");
 const curscore1=document.querySelector("#current--0");
@@ -10,6 +11,7 @@ const pl1 = document.querySelector(".player--0");
 const pl2 = document.querySelector(".player--1");
 let player=1;
 let end=0;
+/*Flag to determine the user and if the game is end or not*/
 function reset(){
 end=0;
 player= 1;
@@ -23,20 +25,23 @@ pl1.classList.remove('player--winner');
 pl2.classList.remove('player--winner');
 }
 reset();
+/*Resetting everything*/
 
 rolld.addEventListener('click',function(){
-	if(end ===0){
-	let secretn = Math.trunc(Math.random()*6+1);
-	let x='dice-'+String(secretn)+'.png';
-	document.getElementById("imgID").src = x;
-	if (player===1){
+	if(end ===0){ //To disable the button if the game is end.
+	let secretn = Math.trunc(Math.random()*6+1); //Creating random number between 1~6
+	let x='dice-'+String(secretn)+'.png';  //making a string for a Dice.png ex) dice-3.png
+	document.getElementById("imgID").src = x; //changing dice img
+	if (player===1){  //If player 1
 		if(secretn === 1){
 			curscore1.textContent = 0;
 			player= 2;
 			pl1.classList.remove('player--active');
 			pl2.classList.add('player--active');
+			/*If the dice number is 1, it resets the current score and switch to player 2*/
 		}else{
 			curscore1.textContent=Number(curscore1.textContent)+secretn;
+			//Else, dice number will be added to current score.
 		}
 	}
 	else if(player===2){
@@ -53,16 +58,16 @@ rolld.addEventListener('click',function(){
 
 })
 hold.addEventListener('click',function(){
-	if(end===0){
+	if(end===0){ //Check if the game is end or not.
 	if (player===1){
-		score1.textContent=Number(score1.textContent)+Number(curscore1.textContent);
-		curscore1.textContent=0;
-		pl1.classList.remove('player--active');
-		pl2.classList.add('player--active');
+		score1.textContent=Number(score1.textContent)+Number(curscore1.textContent); //Add current score to total score when the user press hold.
+		curscore1.textContent=0;  //resets the current score
+		pl1.classList.remove('player--active'); 
+		pl2.classList.add('player--active');  //switching players
 		if(Number(score1.textContent)>= 100){
 			pl1.classList.add('player--winner');
 			end=1;
-		}
+		} //If the score goes higher than 100, this player will win and change end=1 to indicate that the game is end, disabling all buttons.
 		player =2;
 	}else if (player ===2){
 		score2.textContent=Number(score2.textContent)+Number(curscore2.textContent);
@@ -79,4 +84,4 @@ hold.addEventListener('click',function(){
 
 	
 })
-newg.addEventListener('click',reset)
+newg.addEventListener('click',reset) //reseting the game
